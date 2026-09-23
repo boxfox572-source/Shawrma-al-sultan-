6cb425603062aa216282b742788b6eee2d0784d4
boxfox572-source <boxfox572@gmail.com>
2026-09-23 08:42:16 +0300

    Fix icon paths in service worker

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ const ASSETS_TO_CACHE = [
   "./data.js",
   "./promo_data.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  ".icon-192.png",
+  ".icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
