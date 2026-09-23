1911dffaa4077c261970739fa8b23fbd367638f6
boxfox572-source <boxfox572@gmail.com>
2026-09-23 07:29:32 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "soltan-cache-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./data.js",
+  "./promo_data.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).catch(() => caches.match("./index.html"))
+      );
+    })
+  );
+});
